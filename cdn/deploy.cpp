45d3c32fc5d8127aa4d57ff311e97f95aaaf249d
yurikaka <yurikaka417@gmail.com>
2017-03-16 17:42:42 +0800

    basic solve

--- cdn/deploy.cpp
+++ cdn/deploy.cpp
@@ -1,12 +1,23 @@
 #include "deploy.h"
 #include <stdio.h>
+#include <string.h>
 
 //你要完成的功能总入口
 void deploy_server(char * topo[MAX_EDGE_NUM], int line_num,char * filename)
 {
-
+    char out[55000] = "";
+    char temp[55000] = "";
+    int a,b,c;
+    int num_network, num_edge, num_custom;
+    sscanf(topo[0],"%d%d%d",&num_network,&num_edge,&num_custom);
+    sprintf(out,"%d\n\n",num_custom);
+    for (int i = line_num-num_custom; i < line_num; ++i) {
+        sscanf(topo[i],"%d%d%d",&a,&b,&c);
+        sprintf(temp,"%d %d %d\n",b,a,c);
+        strcat(out,temp);
+    }
 	// 需要输出的内容
-	char * topo_file = (char *)"17\n\n0 8 0 20\n21 8 0 20\n9 11 1 13\n21 22 2 20\n23 22 2 8\n1 3 3 11\n24 3 3 17\n27 3 3 26\n24 3 3 10\n18 17 4 11\n1 19 5 26\n1 16 6 15\n15 13 7 13\n4 5 8 18\n2 25 9 15\n0 7 10 10\n23 24 11 23";
+	char * topo_file = (char *)out;
 
 	// 直接调用输出文件的方法输出到指定文件中(ps请注意格式的正确性，如果有解，第一行只有一个数据；第二行为空；第三行开始才是具体的数据，数据之间用一个空格分隔开)
 	write_result(topo_file, filename);
