ca3ee08b8d0ccd35e80dc7b8b993b28e15af2336
yurikaka <yurikaka417@gmail.com>
2017-03-23 21:33:56 +0800

    modify build script

--- build.sh
+++ build.sh
@@ -11,19 +11,29 @@ then
     exit -1
 fi
 
-tmp=$(cmake --version 2>&1)
-if [ $? -ne 0 ]
-then
-    echo "ERROR: You should install cmake(2.8 or later) first."
-    echo "  Please run 'sudo apt-get install cmake' to install it."
-    echo "  or goto https://cmake.org to download and install it."
-    exit
-fi
+# tmp=$(cmake --version 2>&1)
+# if [ $? -ne 0 ]
+# then
+#     echo "ERROR: You should install cmake(2.8 or later) first."
+#     echo "  Please run 'sudo apt-get install cmake' to install it."
+#     echo "  or goto https://cmake.org to download and install it."
+#     exit
+# fi
 
 cp cdn/deploy.cpp SDK-gcc/cdn/deploy.cpp
 cp cdn/deploy.h SDK-gcc/cdn/deploy.h
 cd SDK-gcc
-sh build.sh
+
+rm -fr bin
+mkdir bin
+rm -fr build
+mkdir build
+cd build
+cmake ../cdn
+make
+
+cd ..
+tar -zcPf cdn.tar.gz *
 
 mv cdn.tar.gz ../
 touch bin/.gitkeep
