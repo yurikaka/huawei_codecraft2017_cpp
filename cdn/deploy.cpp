205170a0db071916b5e11c859f5cbc4d48d0e24a
yurikaka <yurikaka417@gmail.com>
2017-03-28 23:45:39 +0800

    delete useless edges

--- cdn/deploy.cpp
+++ cdn/deploy.cpp
@@ -8,6 +8,8 @@ using namespace std;
 typedef struct _edge{
     int cab;
     int cost;
+    int flow;
+    int t_flow;
 }edge;
 
 typedef struct _customer{
@@ -20,29 +22,53 @@ void deploy_server(char * topo[MAX_EDGE_NUM], int line_num,char * filename)
 {
     char out[55000] = "";
     char temp[55000] = "";
-    int a,b,c,d;
+    int a,b,c,d,i,j,count;
     int num_network, num_edge, num_custom,cost_server;
     sscanf(topo[0],"%d%d%d",&num_network,&num_edge,&num_custom);
     sscanf(topo[2], "%d", &cost_server);
     map<int,edge> edges[num_network];
-    for (int i = 4; i < 4 + num_edge; ++i){
+    for (i = 4; i < 4 + num_edge; ++i){
         sscanf(topo[i],"%d%d%d%d",&a,&b,&c,&d);
-        edges[a][b] = {c,d};
-        edges[b][a] = {c,d};
+        edges[a][b] = {c,d,0,0};
+        edges[b][a] = {c,d,0,0};
     }
     customer customers[num_custom];
     int answer[num_custom];
     sprintf(out,"%d\n\n",num_custom);
-    for (int i = line_num-num_custom; i < line_num; ++i) {
+    for (i = line_num-num_custom; i < line_num; ++i) {
         sscanf(topo[i],"%d%d%d",&a,&b,&c);
         customers[a] = {b,c};
         answer[a] = b;
     }
+    //删除无用边
+    do {
+        count = 0;
+        for (i = 0; i < num_network; ++i) {
+            if (edges[i].size() == 1) {
+                for (j = 0; j < num_custom; ++j) {
+                    if (customers[j].no_network == i)
+                        break;
+                }
+                if (j == num_custom) {
+                    edges[edges[i].begin()->first].erase(i);
+                    edges[i].erase(edges[i].begin());
+                    ++count;
+                }
+            }
+        }
+    } while (count > 0);
+
     map<int,edge>::iterator it;
-    for (int i = 0; i < num_custom; ++i){
+//    //输出所有边（测试）
+//    for (i = 0; i < num_network; ++i) {
+//        for (it = edges[i].begin(); it != edges[i].end(); ++it) {
+//            printf("%d %d %d %d %d %d\n", i, it->first, it->second.cab, it->second.cost, it->second.flow, it->second.t_flow);
+//        }
+//    }
+    for (i = 0; i < num_custom; ++i){
         if (answer[i] != customers[i].no_network)
             continue;
-        for (int j = 0; j < num_custom; ++j){
+        for (j = 0; j < num_custom; ++j){
             if (i == j)
                 continue;
             it = edges[customers[i].no_network].find(customers[j].no_network);
@@ -61,7 +87,7 @@ void deploy_server(char * topo[MAX_EDGE_NUM], int line_num,char * filename)
             }
         }
     }
-    for (int i = 0; i < num_custom; ++i){
+    for (i = 0; i < num_custom; ++i){
         if (answer[i] == customers[i].no_network){
             sprintf(temp,"%d %d %d\n",answer[i],i,customers[i].demand);
             strcat(out,temp);
