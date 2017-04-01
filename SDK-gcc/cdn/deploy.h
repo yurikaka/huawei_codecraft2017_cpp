b91633bd0ad15f1fc25d691f4b28bdbfe1f1fce4
yurikaka <yurikaka417@gmail.com>
2017-04-01 10:05:09 +0800

    temporary merge output

--- SDK-gcc/cdn/deploy.h
+++ SDK-gcc/cdn/deploy.h
@@ -2,9 +2,37 @@
 #define __ROUTE_H__
 
 #include "lib_io.h"
+#include <stdio.h>
+#include <string.h>
+#include <map>
+#include <limits.h>
+#include <queue>
+#include <memory.h>
+#include <unordered_set>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+typedef struct _edge{
+    int cab;
+    int cost;
+    int flow;
+    int t_flow;
+}edge;
+
+typedef struct _customer_server{
+    int no_network_usage;
+    int demand_usage_t;
+}customer_server;
+
+typedef struct _cost_flow{
+    int cost;
+    int flow;
+}cost_flow;
 
 void deploy_server(char * graph[MAX_EDGE_NUM], int edge_num, char * filename);
 
-	
 
 #endif
