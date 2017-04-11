bb975e95d23c9008badddb5724a57f48a939a0b6
yurikaka <yurikaka417@gmail.com>
2017-04-11 19:09:25 +0800

    base solve

--- cdn/deploy.cpp
+++ cdn/deploy.cpp
@@ -1,12 +1,45 @@
 #include "deploy.h"
 #include <stdio.h>
 
+char out[550000] = "";
+char out_tmp[550000] = "";
+
 //你要完成的功能总入口
 void deploy_server(char * topo[MAX_EDGE_NUM], int line_num,char * filename)
 {
-
+	int network_num,edge_num,customer_num,level_num,i,j,a,b,c,d;
+	sscanf(topo[0],"%d%d%d",&network_num,&edge_num,&customer_num);
+	level_num = line_num - network_num - edge_num - customer_num -5;
+	level levels[level_num];
+	for (i = 0; i < level_num; ++i){
+		sscanf(topo[i+2],"%d%d%d",&a,&b,&c);
+		levels[i] = {b,c};
+	}
+	int node_cost[network_num];
+	for (i = 3 + level_num; i < network_num + level_num + 3; ++i){
+		sscanf(topo[i],"%d%d",&a,&b);
+		node_cost[a] = b;
+	}
+	map<int,edge> edges[network_num];
+	for (i = 4 + level_num + network_num; i < 4 + level_num + network_num + edge_num; ++i){
+		sscanf(topo[i],"%d%d%d%d",&a,&b,&c,&d);
+		edges[a][b] = {c,d};
+		edges[b][a] = {c,d};
+	}
+	customer customers[customer_num];
+	sprintf(out,"%d\n\n",customer_num);
+	for (i = 5 + level_num + network_num + edge_num; i < 5 + level_num + network_num + edge_num + customer_num; ++i){
+		sscanf(topo[i],"%d%d%d",&a,&b,&c);
+		customers[a] = {b,c};
+		for (j = 0; j < level_num; ++j){
+			if (levels[j].output >= c)
+				break;
+		}
+		sprintf(out_tmp,"%d %d %d %d\n",b,a,c,j);
+		strcat(out,out_tmp);
+	}
 	// 需要输出的内容
-	char * topo_file = (char *)"17\n\n0 8 0 20\n21 8 0 20\n9 11 1 13\n21 22 2 20\n23 22 2 8\n1 3 3 11\n24 3 3 17\n27 3 3 26\n24 3 3 10\n18 17 4 11\n1 19 5 26\n1 16 6 15\n15 13 7 13\n4 5 8 18\n2 25 9 15\n0 7 10 10\n23 24 11 23";
+	char * topo_file = (char *) out;
 
 	// 直接调用输出文件的方法输出到指定文件中(ps请注意格式的正确性，如果有解，第一行只有一个数据；第二行为空；第三行开始才是具体的数据，数据之间用一个空格分隔开)
 	write_result(topo_file, filename);
--- cdn/deploy.h
+++ cdn/deploy.h
@@ -2,6 +2,26 @@
 #define __ROUTE_H__
 
 #include "lib_io.h"
+#include <string.h>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+typedef struct _level{
+    int output;
+    int cost;
+}level;
+
+typedef struct _customer{
+    int node;
+    int demand;
+}customer;
+
+typedef struct _edge{
+    int cab;
+    int price;
+}edge;
 
 void deploy_server(char * graph[MAX_EDGE_NUM], int edge_num, char * filename);
 
