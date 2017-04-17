94fff33c5d891dc4799aa47b654cf65769d271af
yurikaka <yurikaka417@gmail.com>
2017-04-17 15:35:17 +0800

    move->delete->push

--- SDK-gcc/cdn/deploy.cpp
+++ SDK-gcc/cdn/deploy.cpp
@@ -1,47 +1,157 @@
 #include "deploy.h"
-#include <stdio.h>
 
-char out[550000] = "";
-char out_tmp[550000] = "";
+#define PRINT_COST
+#define PRINT_TIME
+
+char out[550000];
+char temp[550000];
+int all_cost = INT32_MAX;
+int current_cost;
+
+int min_node_cost = INT32_MAX;
+
+vector <pair<int,int>> best_answer;
+vector <int> DirectNode;
+
+extern const int NODEMAX, EDGEMAX, INF;
+
+unordered_map<int,pair<int,int>> Level;
+int positionPrice[NODEMAX + 10];
+
+MCMF MCF;
+
+/*************************************************/
+
+//返回当前经过的时间
+int return_time()
+{
+	struct timeb rawtime;
+	ftime(&rawtime);
+	static bool first = true;
+	static int ms;
+	static unsigned long s;
+	if (first) {
+		ms = rawtime.millitm;
+		s = rawtime.time;
+		first = false;
+	}
+	int out_ms = rawtime.millitm - ms;
+	unsigned long out_s = rawtime.time - s;
+	if (out_ms < 0)
+	{
+		out_ms += 1000;
+		out_s -= 1;
+	}
+	return out_s;
+}
+
+bool cmp(pair<int, int> p, pair<int, int> q) {
+	return p.second < q.second;
+}
 
 //你要完成的功能总入口
-void deploy_server(char * topo[MAX_EDGE_NUM], int line_num,char * filename)
+void deploy_server(char * topo[MAX_EDGE_NUM], int line_num, char * filename)
 {
-	int network_num,edge_num,customer_num,level_num,i,j,a,b,c,d;
-	sscanf(topo[0],"%d%d%d",&network_num,&edge_num,&customer_num);
-	level_num = line_num - network_num - edge_num - customer_num -5;
-	level levels[level_num];
-	for (i = 0; i < level_num; ++i){
-		sscanf(topo[i+2],"%d%d%d",&a,&b,&c);
-		levels[i] = {b,c};
-	}
-	int node_cost[network_num];
-	for (i = 3 + level_num; i < network_num + level_num + 3; ++i){
-		sscanf(topo[i],"%d%d",&a,&b);
-		node_cost[a] = b;
-	}
-	map<int,edge> edges[network_num];
-	for (i = 4 + level_num + network_num; i < 4 + level_num + network_num + edge_num; ++i){
-		sscanf(topo[i],"%d%d%d%d",&a,&b,&c,&d);
-		edges[a][b] = {c,d};
-		edges[b][a] = {c,d};
-	}
-	customer customers[customer_num];
-	sprintf(out,"%d\n\n",customer_num);
-	for (i = 5 + level_num + network_num + edge_num; i < 5 + level_num + network_num + edge_num + customer_num; ++i){
-		sscanf(topo[i],"%d%d%d",&a,&b,&c);
-		customers[a] = {b,c};
-		for (j = 0; j < level_num; ++j){
-			if (levels[j].output >= c)
-				break;
-		}
-		sprintf(out_tmp,"%d %d %d %d\n",b,a,c,j);
-		strcat(out,out_tmp);
-	}
-	// 需要输出的内容
-	char * topo_file = (char *) out;
+	vector<pair<int, int>> servers;
+	vector<int> answer;
+	return_time();
+	MCF.clear();
+	MCF.buildGraph(topo);
 
+	MCF.getTotalCost(DirectNode, 0);
+	printf("direct cost is %d \n", all_cost);
+
+	servers = best_answer;
+	for (vector<pair<int, int>>::iterator it = servers.begin(); it != servers.end(); ++it) {
+		bool flag = true;
+		int cost_flow = MCF.edge[MCF.server2edge[it->first] ^ 1].flow;
+		while (flag && positionPrice[it->first] != min_node_cost) {
+			flag = false;
+			for (int i = MCF.G[it->first]; i; i = MCF.edge[i].next) {
+				if (i % 2 == 1)
+					continue;
+				if (MCF.edge[i].to == MCF.vSink || MCF.edge[i].to == MCF.vSource)
+					continue;
+				if (MCF.edge[i].flow + MCF.edge[i ^ 1].flow >= cost_flow) {
+					if (MCF.edge[i].cost * cost_flow <= positionPrice[it->first] - positionPrice[MCF.edge[i].to]) {
+						*it = pair<int, int>(MCF.edge[i].to, it->second);
+						flag = true;
+						break;
+					}
+				}
+			}
+		}
+	}
+	answer.clear();
+	for (vector<pair<int, int>>::iterator it = servers.begin(); it != servers.end(); ++it) {
+		if (find(answer.begin(), answer.end(), it->first) == answer.end())
+			answer.push_back(it->first);
+	}
+	MCF.getTotalCost(answer, 0);
+	printf("cost = %d\n", all_cost);
+
+	int num_sround_min = 5;
+	int num_sround;
+	pair<int, int> tmp_first;
+	pair<int, int> tmp_back;
+	while (num_sround_min >= 1) {
+		servers = best_answer;
+		sort(servers.begin(), servers.end(), cmp);
+		tmp_back = servers.back();
+		while (servers.front() != tmp_back) {
+			tmp_first = servers.front();
+			servers.erase(servers.begin());
+			answer.clear();
+			for (vector<pair<int, int>>::iterator it = servers.begin(); it != servers.end(); ++it) {
+				answer.push_back(it->first);
+			}
+			num_sround = 0;
+			for (int i = MCF.G[tmp_first.first]; i; i = MCF.edge[i].next) {
+				if (i % 2 == 1)
+					continue;
+				if (MCF.edge[i].to == MCF.vSink || MCF.edge[i].to == MCF.vSource)
+					continue;
+				if (find(answer.begin(), answer.end(), MCF.edge[i].to) != answer.end())
+					++num_sround;
+			}
+			if (num_sround >= num_sround_min) {
+				MCF.getTotalCost(answer, 0);
+				if (current_cost == -1)
+					servers.push_back(tmp_first);
+			} else {
+				servers.push_back(tmp_first);
+			}
+			//printf("num of servers = %lu, cost = %d\n", answer.size(), all_cost);
+		}
+		--num_sround_min;
+	}
+
+	servers = best_answer;
+	answer.clear();
+	for (vector<pair<int, int>>::iterator it = servers.begin(); it != servers.end(); ++it) {
+		answer.push_back(it->first);
+	}
+	double min_use = 0.00;
+	do {
+		MCF.getTotalCost(answer, min_use);
+		min_use += 0.01;
+	} while (current_cost != -1);
+	printf("cost = %d\n", all_cost);
+
+	MCF.printAllPath();
+	sprintf(temp, "%d\n\n", MCF.flowCnt);
+	strcat(temp, out);
+
+// 需要输出的内容
+	char * topo_file = (char *) temp;
+
+#ifdef PRINT_COST
+    printf("server num = %d\n",best_answer.size());
+	printf("all cost = %d\n",all_cost);
+#endif
 	// 直接调用输出文件的方法输出到指定文件中(ps请注意格式的正确性，如果有解，第一行只有一个数据；第二行为空；第三行开始才是具体的数据，数据之间用一个空格分隔开)
 	write_result(topo_file, filename);
-
+#ifdef PRINT_TIME
+	printf("now time is %d\n",return_time());
+#endif
 }
--- cdn/deploy.h
+++ cdn/deploy.h
@@ -4,24 +4,548 @@
 #include "lib_io.h"
 #include <string.h>
 #include <map>
+#include <limits.h>
+#include <stdio.h>
+#include <queue>
+#include <memory.h>
+#include <unordered_set>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <stdint.h>
+
+
+
+//for return time
+#include <assert.h>
+#include <time.h>
+#include <sys/timeb.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
 
 using namespace std;
 
-typedef struct _level{
-    int output;
-    int cost;
-}level;
+/* sj
 
-typedef struct _customer{
-    int node;
-    int demand;
-}customer;
+/*
+输出文件得限制（并不是输入文件得限制）
+1. 网络路径数量不得超过300000条。
+2. 单条路径的节点数量不得超过10000个。
+3. 不同网络路径可按任意先后顺序输出。
+4. 网络节点ID与消费节点ID的数值必须与输入文件相符合，如果ID数值不存在于输入文件中，则将被视为无效结果。
+5. 文本文件中出现的所有数值必须为大于等于0的整数，数值大小不得超过1000000。
+*/
 
-typedef struct _edge{
-    int cab;
-    int price;
-}edge;
+//todo 需要注意EDGEMAX 得值，最好在读取文件得过程中重新分配一次 INF的设置还需要稍微琢磨一下(比如出题人的数据量)
+extern char out[550000];
+extern char temp[550000];
+extern int all_cost;
+extern int current_cost;
+
+extern vector <pair<int,int>> best_answer;
+extern vector <int> DirectNode;
+
+const int NODEMAX = 10005, EDGEMAX = 20000, INF = 0x3f3f3f3f ;
+
+extern unordered_map<int,pair<int,int>> Level;
+extern int positionPrice[NODEMAX + 10];
+
+//edge 的定义
+struct Edge{
+    Edge(){};
+//  from -- > to   flow为当前流量， cost为单位费用， next为from开始得下一条边
+    int to, flow, cost, next;
+    Edge(int _to, int _flow, int _cost,int _next){
+        to = _to;
+        flow = _flow;
+        cost = _cost;
+        next = _next;
+    };
+};
+
+//最小费用最大流
+struct MCMF{
+
+/*+++++++++++++++节点数量信息++++++++++++++++++*/
+    int nodeNumber,edgeNumber,consumerNumber;
+    int vSource,vSink; //虚拟源和虚拟汇
+    int needSum; // 官方需要的总流量
+    int currentFlows; // 当前部署下的总流量
+    int edgeorder; //记录当前加到第几条边了
+    int flowCnt = 0; // 打印流时一打印了多少条路径
+    int solveTimes = 0; // 记录一共调用了求解多少次
+    int maxBandWidth;
+    int levelNum = 0;
+//  todo 这里分几个等级
+    int deployCost;
+//  todo 每个节点也需要部署费用
+    ;
+
+
+
+    int G[NODEMAX];
+    Edge edge[EDGEMAX];
+    int Net2Consumer[NODEMAX];
+    unordered_map <int,int> server2level;
+
+/*+++++++++++++++求解最小流全局变量以及各种辅助变量++++++++++++++++++*/
+    queue<int> Q;
+    int dist[NODEMAX + 10];
+    bool inQueue[NODEMAX +10];
+    int preV[NODEMAX + 10]; //回溯打印节点需要
+    int preE[NODEMAX + 10]; //回溯打印节点需要
+    unordered_map<int,int>server2edge;
+
+
+/*+++++++++++++++添加边前向星星++++++++++++++++++*/
+    void addEdge(int from,int to,int capacity,int cost){
+
+        edge[++edgeorder] = Edge(to,capacity,cost,G[from]);
+        G[from] = edgeorder;
+        edge[++edgeorder] = Edge(from,0,-cost,G[to]);
+        G[to] = edgeorder;
+
+    }
+
+
+
+/*+++++++++++++++建图only once++++++++++++++++++*/
+
+    void buildGraph(char *topo[EDGEMAX]){
+        int pos = 0;
+        memset(G, 0, sizeof(G));
+
+        needSum = 0;
+        // 之所以初始为1是为了后面的奇偶
+        edgeorder = 1;
+        // 读取节点数量
+        sscanf(topo[pos],"%d %d %d",&nodeNumber,&edgeNumber,&consumerNumber);
+
+
+        // 源点汇点初始
+        vSink = nodeNumber + 1;
+        vSource = nodeNumber;
+
+        //部署费用需要修改
+        int level = 0,bandwidth = 0,price = 0;
+        for(pos = 2; pos < 12; ++pos){
+            if(topo[pos][0] == '\n' || topo[pos][0] == '\r' || topo[pos][0] == '\0') {
+                maxBandWidth = bandwidth;
+                break;
+            }
+            sscanf(topo[pos],"%d%d%d",&level,&bandwidth,&price);
+            Level[pos-2].first = bandwidth;
+            Level[pos-2].second = price;
+        }
+
+        pos++;
+
+        //地点费用
+        int postion;
+        for (int i = 0; i < nodeNumber; ++i) {
+            sscanf(topo[i + pos], "%d%d", &postion, &price);
+            positionPrice[postion] = price;
+        }
+        pos += nodeNumber +1;
+
+        for (int i = 0; i < edgeNumber; ++i) {
+            int _from,_to,_flow,_cost;
+            sscanf(topo[i + pos],"%d %d %d %d",&_from,&_to,&_flow,&_cost);
+
+            addEdge(_from,_to,_flow,_cost);
+            addEdge(_to,_from,_flow,_cost);
+
+        }
+        pos += edgeNumber + 1;
+
+        bool d = DirectNode.empty();
+        for (int i = 0; i < consumerNumber; ++i) {
+            int consumer,netnode,need;
+            sscanf(topo[i + pos],"%d %d %d",&consumer,&netnode,&need);
+            if(d){
+                DirectNode.push_back(netnode);
+            }
+            addEdge(netnode,vSink,need,0);
+            needSum += need;
+            Net2Consumer[netnode] = consumer;
+        }
+    }
+
+
+/*+++++++++++++++建超级源点++++++++++++++++++*/
+    void buildSource(vector<int> servers){
+        for(auto server:servers){
+            // todo 这里流量不是无限大注意更改，cost也注意修改
+            if(server2edge.count(server)){
+                int tmpEdgeNum = server2edge[server];
+                //todo 实际中肯定不是无穷大
+                edge[tmpEdgeNum].flow = maxBandWidth;
+                edge[tmpEdgeNum ^ 1].flow = 0;
+            }else{
+                //todo 实际中肯定不是无穷大
+                addEdge(vSource,server,maxBandWidth,0);
+                server2edge[server] = edgeorder - 1;
+            }
+        }
+    }
+
+
+    void buildSource(vector<pair<int,int>> servers){
+        for(auto server:servers){
+            // todo 这里流量不是无限大注意更改，cost也注意修改
+            if(server2edge.count(server.first)){
+                int tmpEdgeNum = server2edge[server.first];
+                //todo 实际中肯定不是无穷大
+                edge[tmpEdgeNum].flow = Level[server.second].first;
+                edge[tmpEdgeNum ^ 1].flow = 0;
+            }else{
+                //todo 实际中肯定不是无穷大
+                addEdge(vSource,server.first,Level[server.second].first,0);
+                server2edge[server.first] = edgeorder - 1;
+            }
+        }
+    }
+
+
+
+/*+++++++++++++++clear上次的计算内容，图置为初始情况(源点)++++++++++++++++++*/
+    void clear(){
+
+        currentFlows = 0;
+        flowCnt = 0;
+        memset(dist,63,sizeof(dist));
+        memset(inQueue,0,sizeof(inQueue));
+        memset(preV,0,sizeof(preV));
+        memset(preE,0,sizeof(preE));
+
+        //memset(edge,0,sizeof(edge));
+        //todo 所有边置为读进来的情况，注意有没有需要特殊处理的情况，比如汇点和源点
+        for(int i = 2; i <= edgeorder; i += 2){
+            edge[i].flow += edge[i^1].flow;
+            edge[i^1].flow = 0;
+        }
+
+        for(auto it = server2edge.begin(); it != server2edge.end(); ++it){
+            int tmpEdgeNum = (*it).second;
+            edge[tmpEdgeNum].flow = 0;
+            edge[tmpEdgeNum ^ 1].flow = 0;
+        }
+    }
+
+/*+++++++++++++++clear上次的计算内容，顺便根据要删除的和不删除的点建好新的边++++++++++++++++++*/
+
+    void deleteServers(vector<int> toDeletServers){
+        for(auto server: toDeletServers){
+            if(server2edge.count(server)){
+                int tmpEdgeNum = server2edge[server];
+                edge[tmpEdgeNum].flow = 0;
+                edge[tmpEdgeNum ^ 1].flow = 0;
+            }else{
+                continue;
+            }
+        }
+    }
+
+    void addServers(vector<int> toAddServers){
+        for(auto server: toAddServers){
+            if(server2edge.count(server)){
+                int tmpEdgeNum = server2edge[server];
+                //todo  maxBandWidth
+                edge[tmpEdgeNum].flow = maxBandWidth;
+                edge[tmpEdgeNum ^ 1].flow = 0;
+            }else{
+                //todo maxBandWidth
+                addEdge(vSource,server,maxBandWidth,0);
+                server2edge[server] = edgeorder - 1;
+            }
+        }
+    }
+
+    void clear(vector<int> toDeletServers,vector<int> toAddServers){
+
+        currentFlows = 0;
+        flowCnt = 0;
+        memset(dist,63,sizeof(dist));
+        memset(inQueue,0,sizeof(inQueue));
+        memset(preV,0,sizeof(preV));
+        memset(preE,0,sizeof(preE));
+
+        //todo 所有边置为读进来的情况，注意有没有需要特殊处理的情况，比如汇点和源点
+        for(int i = 2; i <= edgeorder; i += 2){
+            edge[i].flow += edge[i^1].flow;
+            edge[i^1].flow = 0;
+        }
+
+        deleteServers(toDeletServers);
+        addServers(toAddServers);
+    }
+
+
+
+
+
+/*+++++++++++++++是否有一条路径++++++++++++++++++*/
+    bool hasPath(){
+        memset(dist,63,sizeof(dist));
+
+        dist[vSource] = 0;
+        while (!Q.empty()) Q.pop();
+        Q.push(vSource);
+        inQueue[vSource] = true;
+
+        while(!Q.empty()){
+            int from = Q.front();
+            Q.pop();
+            for(int i = G[from];i; i = edge[i].next){
+                if(edge[i].flow > 0 && dist[from] + edge[i].cost < dist[edge[i].to]){
+                    dist[edge[i].to] = dist[from] + edge[i].cost;
+                    preV[edge[i].to] = from;
+                    preE[edge[i].to] = i;
+                    if(!inQueue[edge[i].to]) {
+                        inQueue[edge[i].to] = true;
+                        Q.push(edge[i].to);
+                    }
+                }
+            }
+            inQueue[from] = false;
+        }
+        return dist[vSink] < INF;
+    }
+
+
+/*+++++++++++++++返回当前路径增广的成本++++++++++++++++++*/
+
+    int augment(){
+        int from = vSink;
+        int delta = INF;
+        while(from != vSource){
+            if (edge[preE[from]].flow < delta) delta = edge[preE[from]].flow;
+            from = preV[from];
+        }
+        from = vSink;
+        while (from != vSource){
+
+            edge[preE[from]].flow -= delta;
+            edge[preE[from] ^ 1].flow += delta;
+            from = preV[from];
+        }
+        currentFlows += delta;
+        return dist[vSink] * delta;
+    }
+
+/*+++++++++++++++判断是否是可行流++++++++++++++++++*/
+    bool isFeasibleFlow(){
+
+        return needSum == currentFlows;
+    }
+
+/*+++++++++++++++求解可行流++++++++++++++++++*/
+    int solve(){
+        solveTimes ++;
+        int cur = 0,ans = 0;
+        while (hasPath()){
+            cur += augment();
+            if(cur > ans) ans = cur;
+        }
+        return ans;
+    }
+
+/*+++++++++++++++打印一条路径++++++++++++++++++*/
+    //一般最后一步才会去调用 调用方式是while(printOnePath);
+    bool printOnePath(int sink){
+        int minimum = INF;
+        bool haspath = false;
+        vector<int> tmpPre;
+        vector<int> tmpPree;
+        int from = vSink;
+        while(from != vSource) {
+            for (int i = G[from]; i; i = edge[i].next) {
+                if ((i & 1) == 0) continue;
+                if (edge[i].flow > 0) {
+                    haspath = true;
+                    tmpPre.push_back(edge[i].to);
+                    minimum = min(minimum,edge[i].flow);
+                    tmpPree.push_back(i);
+                    from = edge[i].to;
+                    break;
+                }
+            }
+            if (minimum == INF) break;
+        }
+        if (haspath) {
+            for (auto edgen : tmpPree) {
+                edge[edgen].flow -= minimum;
+            }
+            reverse(tmpPre.begin(),tmpPre.end());
+            int last;
+            int server = tmpPre[1];
+            for (auto v:tmpPre) {
+                // todo 需要处理边的情况
+                if(v == vSource) continue;
+                sprintf(temp,"%d ",v);
+                strcat(out,temp);
+                last = v;
+            }
+            // todo 需要处理输出
+            sprintf(temp,"%d %d %d\n",Net2Consumer[last],minimum,server2level[server]);
+            strcat(out,temp);
+        }
+        return haspath;
+    }
+
+/*+++++++++++++++打印所有路径++++++++++++++++++*/
+    void printAllPath(){
+        for(auto server:best_answer){
+            server2level[server.first] = server.second;
+        }
+        while(printOnePath(vSink))
+            ++flowCnt;
+    }
+
+/*+++++++++++++++打印指定起点的路径++++++++++++++++++*/
+//  while(printSpecificPathFromServer(serverNode));
+    bool printSpecificPathFromServer(int serverNode){
+        int minimum = INF;
+        bool haspath = false;
+        vector<int> tmpPre;
+        vector<int> tmpPree;
+        int from = serverNode;
+        tmpPre.push_back(from);
+        while(from != vSink) {
+            for (int i = G[from]; i; i = edge[i].next) {
+                if ((i & 1) == 1) continue;
+                if (edge[i^1].flow > 0) {
+                    haspath = true;
+                    tmpPre.push_back(edge[i].to);
+                    minimum = min(minimum,edge[i^1].flow);
+                    tmpPree.push_back(i^1);
+                    from = edge[i].to;
+                    break;
+                }
+            }
+            if (minimum == INF) break;
+        }
+        if (haspath) {
+            for (auto edgen : tmpPree) {
+                edge[edgen].flow -= minimum;
+                edge[edgen ^ 1].flow += minimum;
+            }
+
+            int last;
+            for (auto v:tmpPre) {
+                if(v == vSink) continue;
+                cout << v << " ";
+                last = v;
+            }
+            cout << Net2Consumer[last];
+            cout << endl;
+        }
+        return haspath;
+    }
+
+/*+++++++++++++++获取整个cost信息++++++++++++++++++*/
+    //首先默认为最高级，然后根据求得的结果以及百分比，来优化等级信息
+    void getTotalCost(vector<int> servers,double percent){
+
+        clear();
+        // 默认为用的最高级的case
+        buildSource(servers);
+        auto beforeResult = solve();
+        if(!isFeasibleFlow()){
+            current_cost = -1;
+            return;
+        }
+        int beforeTotalCost = 0;
+        int afterTotalCost = 0;
+
+        int server_position_cost = 0;
+        int before_server_level_cost = 0;
+        int after_server_level_cost = 0;
+        // solve之后 每个服务器的出度应该求出来了
+        // 再对所有的服务器看看能降级的是否应该降个级
+        int beforeDegradationCost = 0;
+        // 求出所有节点对应的服务器等级
+        // 同时更改服务器等级 得到newServers
+
+        int decreasePostionCost = 0;
+
+        vector<pair<int,int>> origiServers;
+        vector<pair<int,int>> newServers;
+        for(auto server : servers){
+            int edgeNumber = server2edge[server];
+            int outflows = edge[edgeNumber ^ 1].flow;
+
+            server_position_cost += positionPrice[server];
+            int levelOder = 0;
+
+            for(int i = 0; i < Level.size(); ++i){
+                // 找出等级 并适当的降级
+                if (outflows <= Level[i].first){
+                    origiServers.push_back(make_pair(server,i));
+                    before_server_level_cost += Level[i].second;
+
+                    if(i == 0){
+                        if(outflows > Level[0].first * percent){
+                            newServers.push_back(make_pair(server, i));
+                            after_server_level_cost += Level[i].second;
+                            break;
+                        }
+                        // 因为当前点取消，所以当前的位置价格也会取消
+                        decreasePostionCost += positionPrice[server];
+                        break;
+                    }
+                    if((outflows - Level[i - 1].first) < (Level[i].first - Level[i-1].first) * percent){
+                        newServers.push_back(make_pair(server, i - 1));
+                        after_server_level_cost += Level[i-1].second;
+                    }else{
+                        newServers.push_back(make_pair(server, i));
+                        after_server_level_cost += Level[i].second;
+                    }
+                    break;
+                }
+            }
+        }
+        beforeTotalCost = server_position_cost + before_server_level_cost + beforeResult;
+
+        clear();
+        buildSource(newServers);
+        int afterResult = solve();
+        afterTotalCost = server_position_cost - decreasePostionCost + after_server_level_cost + afterResult;
+
+        if (isFeasibleFlow()) {
+            if (afterTotalCost < beforeTotalCost){
+                if(afterTotalCost < all_cost){
+                    all_cost = afterTotalCost;
+                    best_answer = newServers;
+                }else{
+                    //cout << "expensive" <<endl;
+                }
+            }else{
+                if(beforeTotalCost < all_cost){
+                    all_cost = beforeTotalCost;
+                    best_answer = origiServers;
+                }else{
+                    //cout << "expensive" <<endl;
+                }
+            }
+        } else{
+            current_cost = -1;
+            if(beforeTotalCost < all_cost){
+                all_cost = beforeTotalCost;
+                best_answer = origiServers;
+            }else{
+                //cout << "expensive" <<endl;
+            }
+            clear();
+            buildSource(best_answer);
+            solve();
+        }
+    }
+};
+
+
 
 void deploy_server(char * graph[MAX_EDGE_NUM], int edge_num, char * filename);
 
